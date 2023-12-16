08b15c8e98ae6ad9816d0a1de05534213129c069
Jaishree9898 <jaishree9898@gmail.com>
2023-12-17 00:53:29 +0530

    add

--- javafirst.js
+++ javafirst.js
@@ -1,99 +1,229 @@
-// // // //Arithimetic Operators
+// // // // //Arithimetic Operators
 
-// // // let a=6;
-// // // let b=2;
+// // // // let a=6;
+// // // // let b=2;
 
-// // // console.log("a+b",a+b);
-// // // console.log("a-b",a-b);
-// // // console.log("a*b",a*b);
-// // // console.log("a/b",a/b);
-// // // console.log("a%b",a&b);
-// // // console.log("a**b",a**b);
+// // // // console.log("a+b",a+b);
+// // // // console.log("a-b",a-b);
+// // // // console.log("a*b",a*b);
+// // // // console.log("a/b",a/b);
+// // // // console.log("a%b",a&b);
+// // // // console.log("a**b",a**b);
 
 
-// // // let a=6;
-// // // let b=2;
+// // // // let a=6;
+// // // // let b=2;
 
-// // // console.log("a=b",a,"&b=",b);
+// // // // console.log("a=b",a,"&b=",b);
 
-// // // Assignment Operator
+// // // // Assignment Operator
 
-// // // = += -= %= **=
+// // // // = += -= %= **=
+
+// // // let a=5;
+// // // let b=6;
+
+// // // a += 4; // a=a+4
+// // // a -= 4; // a=a-4
+
+// // // //so on for all operators
+
+// // // Comparision Operator
+
+// // //== equals to
+// // //=== equals to and type
+
+// // //!= not equals to
+// // //!==== not equals to and types
 
 // // let a=5;
 // // let b=6;
 
-// // a += 4; // a=a+4
-// // a -= 4; // a=a-4
+// // console.log("a==b",a==b);
 
-// // //so on for all operators
+// // Logical Operatior
 
-// // Comparision Operator
+// // let a=6;
+// // let b=5;
 
-// //== equals to
-// //=== equals to and type
+// // let cond1 = a > b;
+// // let cond2 = a==b;
 
-// //!= not equals to
-// //!==== not equals to and types
-
-// let a=5;
-// let b=6;
-
-// console.log("a==b",a==b);
-
-// Logical Operatior
-
-// let a=6;
-// let b=5;
-
-// let cond1 = a > b;
-// let cond2 = a==b;
-
-// console.log(cond1 || cond2);
-// console.log(!(a==b));
+// // console.log(cond1 || cond2);
+// // console.log(!(a==b));
 
 
-//Conditional Statements
+// //Conditional Statements
 
-// if Statement
+// // if Statement
 
-// let age=16;
+// // let age=16;
 
-// if (age>18){
-//     console.log("You can vote");
-// }
+// // if (age>18){
+// //     console.log("You can vote");
+// // }
 
-// else{
-//     console.log("You can not vote");
-// }
+// // else{
+// //     console.log("You can not vote");
+// // }
 
 
-// let mode ="dark";
-// let color;
+// // let mode ="dark";
+// // let color;
+
+// // if(mode=="dark"){
+// //     color="black";
+// // }
+
+// // if(mode=="light"){
+// //     color="white";
+// // }
+
+// // console.log(color);
+
+
+// //if else
+
+
+// // let mode ="dark";
+
+// // if(mode=="dark"){
+// //     color="black";
+// // }
+
+// // else{
+// //     color="white";
+// // }
+
+// // console.log(color);
+
+// //odd or even
+
+// // let num=5;
+
+// // if(num%2==0){
+// //     num="Even";
+// // }
+
+// // else{
+// //     num="odd";
+// // }
+
+// // console.log("num is ",num);
+
+// //syntax-->rules
+
+// // else if---
+
+// // let age=78;
+
+// // if(age<18){
+// //     console.log("junior");
+// // }
+
+// // else if(age>60){
+// //     console.log("senior");
+// // }
+
+// // else{
+// //     console.log("middle");
+// // }
+
+// let mode="dark";
 
 // if(mode=="dark"){
 //     color="black";
 // }
 
-// if(mode=="light"){
-//     color="white";
+// else if(mode=="blue"){
+//     color="blue";
 // }
 
-// console.log(color);
+// else if(mode=="pink"){
+//     color="pink";
+// }
+
+// else{
+//     color="white"
+// }
+
+// console.log(color);s
+
+// if(mode=="dark"){
+
+// console.log(mode);
+// }
 
 
-//if else
+//ternary Operator
+// let age=16;
+
+// let result = age>18?"adult":"not adult";
+
+// console.log(result);
 
 
-let mode ="dark";
 
-if(mode=="dark"){
-    color="black";
+// let age=16;
+
+// let result = age>18? console.log("adult"):console.log("not adult");
+
+
+// alert("hello!");
+
+// prompt("hello!");
+
+// let name=prompt("hello!");
+// console.log(name);
+
+
+/* #Get user to input a number using prompt("enter a number"). Check if the number is a multiple of 5 or not  */
+// let num=prompt("Enter a Number");
+// if(num%5==0){
+//     console.log("Number is divided by 5");
+// }
+
+// else{
+//     console.log("Number is not divided by 5");
+// }
+
+
+/* # Write a code which can give grades to students according to thier scores.
+ .90-100,A
+ .70-89,B
+ .60-69,C
+ .50-59,D
+ .0-49,F
+*/
+
+let score=prompt("Enter Scores");
+
+if(score>=90 && score<=100){
+    grade="A";
+}
+
+else if(score>=70 && score<=89){
+    grade="B";
+}
+else if(score>=60 && score<=69){
+    grade="C";
+}
+
+else if(score>=50 && score<=59){
+    grade="D";
+}
+
+else if(score>=0 && score<=49){
+    grade="E"
 }
 
 else{
-    color="white";
+    grade="not defined"
 }
 
-console.log(color);
+console.log(grade);
+
+
+
+
 
