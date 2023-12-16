afe39961ecd90edfea6e866e0c7813b98ee9efe7
Jaishree9898 <jaishree9898@gmail.com>
2023-12-16 21:05:29 +0530

    file

---
+++ javafirst.js
@@ -0,0 +1,99 @@
+// // // //Arithimetic Operators
+
+// // // let a=6;
+// // // let b=2;
+
+// // // console.log("a+b",a+b);
+// // // console.log("a-b",a-b);
+// // // console.log("a*b",a*b);
+// // // console.log("a/b",a/b);
+// // // console.log("a%b",a&b);
+// // // console.log("a**b",a**b);
+
+
+// // // let a=6;
+// // // let b=2;
+
+// // // console.log("a=b",a,"&b=",b);
+
+// // // Assignment Operator
+
+// // // = += -= %= **=
+
+// // let a=5;
+// // let b=6;
+
+// // a += 4; // a=a+4
+// // a -= 4; // a=a-4
+
+// // //so on for all operators
+
+// // Comparision Operator
+
+// //== equals to
+// //=== equals to and type
+
+// //!= not equals to
+// //!==== not equals to and types
+
+// let a=5;
+// let b=6;
+
+// console.log("a==b",a==b);
+
+// Logical Operatior
+
+// let a=6;
+// let b=5;
+
+// let cond1 = a > b;
+// let cond2 = a==b;
+
+// console.log(cond1 || cond2);
+// console.log(!(a==b));
+
+
+//Conditional Statements
+
+// if Statement
+
+// let age=16;
+
+// if (age>18){
+//     console.log("You can vote");
+// }
+
+// else{
+//     console.log("You can not vote");
+// }
+
+
+// let mode ="dark";
+// let color;
+
+// if(mode=="dark"){
+//     color="black";
+// }
+
+// if(mode=="light"){
+//     color="white";
+// }
+
+// console.log(color);
+
+
+//if else
+
+
+let mode ="dark";
+
+if(mode=="dark"){
+    color="black";
+}
+
+else{
+    color="white";
+}
+
+console.log(color);
+
